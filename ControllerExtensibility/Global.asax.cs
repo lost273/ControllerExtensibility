e1324f7d010acf3a2de1f57944154cbe3cf16437
lost273 <dnk273@gmail.com>
2017-10-18 12:04:32 +0300

    18-10-12-03

---
+++ ControllerExtensibility/Global.asax.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using ControllerExtensibility.Infrastructure;
+
+namespace ControllerExtensibility
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            //WebApiConfig.Register(GlobalConfiguration.Configuration);
+            //FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            ControllerBuilder.Current.DefaultNamespaces.Add("MyControllerNamespace");
+            ControllerBuilder.Current.DefaultNamespaces.Add("MyProject.*");
+        }
+    }
+}
